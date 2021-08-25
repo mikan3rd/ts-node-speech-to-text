7466f268952c4511bfa8b5cb5e1cdaf671621d49
mikan3rd <hiroki.ota@zenkigen.jp>
2021-08-25 15:51:16 +0900

    chore: large size audio for gcp

--- src/amivoice.ts
+++ src/amivoice.ts
@@ -5,9 +5,12 @@ import util from "util";
 
 const { AMIVOICE_APPKEY } = process.env;
 
-export const getAmivoiceResult = async (imageFilePath: string) => {
+export const getAmivoiceResult = async (filePath: string) => {
+  if (!AMIVOICE_APPKEY) {
+    throw new Error("AMIVOICE_APPKEY is not set");
+  }
   const url = "https://acp-api.amivoice.com/v1/recognize";
-  const file = fs.createReadStream(imageFilePath);
+  const file = fs.createReadStream(filePath);
   const data = new FormData();
   data.append("a", file);
   const params = { d: "-a-general", u: AMIVOICE_APPKEY };
